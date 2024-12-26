8a64acb456cb119d69a032ebd59c1997af1f02c9
PC1412\quych <caohoanghuy2000@gmail.com>
2024-12-26 21:48:25 +0700

    Day 2

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+import './style.css'
 import * as THREE from 'three' // ES Module import
+import gsap from 'gsap'
+
 
 // Scene
 const scene = new THREE.Scene()
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
 // Object 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
@@ -9,6 +16,40 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff000 })
 const mesh = new THREE.Mesh(geometry, material)
 
 scene.add(mesh)
+
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+// mesh.scale.x = 2 
+// mesh.scale.y = 0.25
+// mesh.scale.z = 0.5
+
+// Group
+// const group = new THREE.Group()
+// group.scale.y = 2
+// group.rotation.y = 0.2
+// scene.add(group)
+
+// const cube1 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: 0xff000 })
+// )
+// cube1.position.x = - 1.5
+// group.add(cube1)
+
+// const cube2 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: 0xff000 })
+// )
+// cube2.position.x = 0
+// group.add(cube2)
+
+// const cube3 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: 0xff000 })
+// )
+// cube3.position.x = 1.5
+// group.add(cube3)
 
 // Size
 const sizes = {
@@ -29,4 +70,23 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Animate
+// let time = Date.now()
+// const clock = new THREE.Clock()
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2})
+const tick = () => {
+    console.log('tick')
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time 
+    // time = currentTime
+    // const elapsedTime = clock.getElapsedTime()
+
+    // mesh.rotation.y += 0.01 * deltaTime
+    // mesh.rotation.x = Math.cos(elapsedTime)
+    // mesh.rotation.y = Math.sin(elapsedTime)
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
